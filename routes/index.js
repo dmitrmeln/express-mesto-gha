dbfdf778b4259d8d2f30ca51eefb47f927d2746c
Dmitrii Melnichuk <melnichuk176@gmail.com>
2023-11-24 15:27:11 +0300

    project 13: autotest fixes

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ const cardsRouter = require('./cards');
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
-router.get('/', (req, res) => res.status(200).send('Backend for MESTO'));
-router.get('*', (req, res) => res
+router.use('/', (req, res) => res.status(200).send('Backend for MESTO'));
+router.use('*', (req, res) => res
   .status(routerError.status)
-  .send(routerError.message));
+  .send({ message: routerError.message }));
 
 module.exports = router;
